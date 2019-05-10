70172008fd93fb8c83289cbf9ba5db4abfdfcf29
vagrant <vagrant@localhost.localdomain>
2019-05-10 11:57:59 +0000

    20190510

--- app/controllers/fav_controller_controller.rb
+++ app/controllers/fav_controller_controller.rb
@@ -1,11 +1,10 @@
 class FavControllerController < ApplicationController
-	before_action :authenticate_user!
-	require 'twitter'
-	
+  before_action :authenticate_user!
+  require 'twitter'
+  
   def index
     twitter_data
     get_tweet
-    sort_test
   end
 
   def twitter_data
@@ -17,24 +16,24 @@ class FavControllerController < ApplicationController
      end
    end
 
-   def sort_test
-    @sort = params[:sort]
-   end
-
   def get_tweet
     @client.user_timeline("ziro264", count:200).each do |tweet|
       @tweet =Tweet.new(user_id: current_user.id, tweet: tweet.text, fav: tweet.favorite_count, tweet_time: tweet.created_at, tweet_id: tweet.id)
       @tweet.save
     end
-    if params[:sort] == "many"
-      @tweets = Tweet.where(user_id: current_user.id).order(fav: "DESC")
-    elsif params[:sort] == "few"
-      @tweets = Tweet.where(user_id: current_user.id).order(fav: "ASC")
-    elsif params[:sort] == "new"
-      @tweets = Tweet.where(user_id: current_user.id).order(tweet_time: "DESC")
-    elsif params[:sort] == "old"
-      @tweets = Tweet.where(user_id: current_user.id).order(tweet_time: "ASC")
+
+    @tweets = Tweet.where(user_id: current_user.id)
+    case params[:sort]
+      when "many"
+        @tweets = @tweets.order(fav: "DESC")
+      when "few"
+        @tweets = @tweets.order(fav: "ASC")
+      when "new"
+        @tweets = @tweets.order(tweet_time: "DESC")
+      when "old"
+        @tweets = @tweets.order(tweet_time: "ASC")
     end
+    render "index"
   end
 
 end
