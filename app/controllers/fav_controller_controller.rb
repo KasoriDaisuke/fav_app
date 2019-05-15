859a7f66b61577bbfd6ff08dc01cd6abf3adfd70
vagrant <vagrant@localhost.localdomain>
2019-05-15 13:35:46 +0000

    20190515

--- app/controllers/fav_controller_controller.rb
+++ app/controllers/fav_controller_controller.rb
@@ -4,25 +4,25 @@ class FavControllerController < ApplicationController
   
   def index
     twitter_data
-    get_tweet
+    # get_tweet
   end
 
   def twitter_data
-     @client = Twitter::REST::Client.new do |config|
-       config.consumer_key         = Rails.application.credentials.twitter_api_key
-       config.consumer_secret      = Rails.application.credentials.twitter_api_secret
-       config.access_token         = Rails.application.credentials.twitter_token
-       config.access_token_secret  = Rails.application.credentials.twitter_token_secret
-     end
-   end
-
-  def get_tweet
-    @client.user_timeline("ziro264", count:200).each do |tweet|
-      @tweet =Tweet.new(user_id: current_user.id, tweet: tweet.text, fav: tweet.favorite_count, tweet_time: tweet.created_at, tweet_id: tweet.id)
-      @tweet.save
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key         = Rails.application.credentials.twitter_api_key
+      config.consumer_secret      = Rails.application.credentials.twitter_api_secret
+      config.access_token         = Rails.application.credentials.twitter_token
+      config.access_token_secret  = Rails.application.credentials.twitter_token_secret
     end
 
-    @tweets = Tweet.where(user_id: current_user.id)
+    User.all.each do |user|
+      @client.user_timeline(user.uid.to_i, count:200).each do |tweet|
+        @tweet =Tweet.new(user_id: user.uid.to_i, tweet: tweet.text, fav: tweet.favorite_count, tweet_time: tweet.created_at, tweet_id: tweet.id)
+        @tweet.save
+      end
+    end
+
+    @tweets = Tweet.where(user_id: 105110241)
     case params[:sort]
       when "many"
         @tweets = @tweets.order(fav: "DESC")
@@ -33,7 +33,5 @@ class FavControllerController < ApplicationController
       when "old"
         @tweets = @tweets.order(tweet_time: "ASC")
     end
-    render "index"
   end
-
 end
